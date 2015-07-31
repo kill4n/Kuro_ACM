ce1a23ab8cc6cf7980907464c46d8560500cf4c1
kill4n <killan@ubu.com>
2015-07-31 11:22:59 -0500

    add OpenCV_joystick Test

---
+++ Test_kuro/Joystick/joy_test/build/CMakeFiles/joy_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/joy_test.dir/main.cpp.o"
+  "joy_test.pdb"
+  "joy_test"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/joy_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
