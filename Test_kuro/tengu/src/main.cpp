30afedd0f8323b5632f88e0d00f13f560a750654
kill4n <crhistian.segura@gmail.com>
2015-08-25 10:50:24 -0500

    din1

--- Test_kuro/tengu/src/main.cpp
+++ Test_kuro/tengu/src/main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 #include <unistd.h>
 
+#include "dynamixel.h"
+
 using namespace std;
+
+
 
 int main()
 {
+    printf("Iniciar ROBOT\r\n");
+int speed=0;
+    while(true)
+    {
+        printf("Defina velocidad:\r\n");
+        cin>>speed;
+
+    }
     return 0;
 }
