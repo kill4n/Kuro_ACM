d28276402c35b7827ada23bf2da046d6d235c33f
killan ubuntu <killan@ubu.com>
2015-10-06 09:33:29 -0500

    Camera Thread OK

---
+++ kuro_Core/librerias/mysemaphore.cpp
@@ -0,0 +1,5 @@
+#include "mysemaphore.h"
+
+MySemaphore::MySemaphore()
+{
+}
---
+++ kuro_Core/librerias/mysemaphore.h
@@ -0,0 +1,10 @@
+#ifndef MYSEMAPHORE_H
+#define MYSEMAPHORE_H
+
+class MySemaphore
+{
+public:
+    MySemaphore();
+};
+
+#endif // MYSEMAPHORE_H
