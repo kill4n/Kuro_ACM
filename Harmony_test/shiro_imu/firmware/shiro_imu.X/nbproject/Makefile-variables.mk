79fafc097e919533a619d85e5f6efa1caefdb023
kill4n <crhistian.segura@gmail.com>
2015-10-19 15:29:32 -0500

    shiro imu

---
+++ Harmony_test/shiro_imu/firmware/shiro_imu.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# shiro_imu_conf configuration
+CND_ARTIFACT_DIR_shiro_imu_conf=dist/shiro_imu_conf/production
+CND_ARTIFACT_NAME_shiro_imu_conf=shiro_imu.X.production.hex
+CND_ARTIFACT_PATH_shiro_imu_conf=dist/shiro_imu_conf/production/shiro_imu.X.production.hex
+CND_PACKAGE_DIR_shiro_imu_conf=${CND_DISTDIR}/shiro_imu_conf/package
+CND_PACKAGE_NAME_shiro_imu_conf=shiroimu.x.tar
+CND_PACKAGE_PATH_shiro_imu_conf=${CND_DISTDIR}/shiro_imu_conf/package/shiroimu.x.tar
