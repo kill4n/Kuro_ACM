3dfccb1d1a56181feb8cff4878613dfb9cbd1074
kill4n <crhistian.segura@gmail.com>
2015-11-09 18:29:41 -0500

    migrate laser

--- kuro_Core/sensores/laser/include/lasercontroller.h
+++ kuro_Core/sensores/laser/include/lasercontroller.h
@@ -1,12 +1,15 @@
 #ifndef LASERCONTROLLER_H
 #define LASERCONTROLLER_H
 
+#include "librerias/thread_aux/include/mythread.h"
 
 class LaserController : public MyThread
 {
 public:
     LaserController();
     ~LaserController();
+    //metodo virtual thread
+    virtual void InternalThreadEntry();
 
 private:
 
