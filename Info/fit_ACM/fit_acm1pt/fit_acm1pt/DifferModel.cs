4bb20720eee82e14e12e6e2fdb78f3d0b8c7c132
kill4n <crhistian.segura@gmail.com>
2015-12-11 11:45:31 -0500

    omni+diff models

---
+++ Info/fit_ACM/fit_acm1pt/fit_acm1pt/DifferModel.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using AX_12;
+
+namespace fit_acm1pt
+{
+    public class DifferModel : IModeloInterface
+    {
+        #region Variables
+        const int numRuedas = 6;
+        List<AX_12_Motor> ax12_Rot;
+        List<AX_12_Motor> ax12_Dir;
+        int _speed, _direction;
+        private int _device_index;
+        public int Device_Index
+        {
+            get { return _device_index; }
+            set { _device_index = value; }
+        }
+        #endregion
+
+
+        public void startModel()
+        {
+            Console.WriteLine("iniciando omnidireccional");
+            for (int i = 0; i < numRuedas; ++i)
+            {
+                ax12_Dir.Add(new AX_12_Motor());
+                ax12_Rot.Add(new AX_12_Motor());
+            }
+            for (int i = 0; i < numRuedas; ++i)
+            {
+                ax12_Dir[i].setDeviceID(_device_index);
+                ax12_Rot[i].setDeviceID(_device_index);
+                ax12_Dir[i].setBaudSpeed(BAUD_RATE.BAUD_1Mbps);
+                ax12_Rot[i].setBaudSpeed(BAUD_RATE.BAUD_1Mbps);
+            }
+
+            for (int i = 0; i < numRuedas; ++i)
+            {
+                ax12_Rot[i].setID(i + 7);
+                ax12_Rot[i].startMotor();
+                ax12_Rot[i].setType(MOTOR_TYPE.WHEEL);
+
+                ax12_Dir[i].setID(i + 1);
+                ax12_Dir[i].startMotor();
+                ax12_Dir[i].setType(MOTOR_TYPE.JOINT);
+            }
+            setSpeed(0);
+            setDirection(0);
+        }
+
+        public void setSpeed(int goalSpeed)
+        {
+            _speed = goalSpeed;
+            for (int i = 0; i < numRuedas; ++i)
+            {
+                if ((i % 2) != 0)
+                    ax12_Rot[i].moveMotor(-_speed);
+                else
+                    ax12_Rot[i].moveMotor(_speed);
+            }
+        }
+
+        public int getSpeed()
+        {
+            return _speed;
+        }
+
+        public void setDirection(int goalDir)
+        {
+            _direction = goalDir;
+            for (int i = 0; i < numRuedas; ++i)
+            {
+                ax12_Rot[i].moveMotor(_direction);
+            }
+        }
+
+        public int getDirection()
+        {
+            return _direction;
+        }
+
+        public void stopModel()
+        {
+            for (int i = 0; i < numRuedas; ++i)
+            {
+                ax12_Rot[i].stopMotor();
+                ax12_Dir[i].stopMotor();
+            }
+        }
+    }
+}
---
+++ Info/fit_ACM/fit_acm1pt/fit_acm1pt/IModeloInterface.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace fit_acm1pt
+{
+    public enum MODELO_TYPE
+    {
+        OMNIDIRECCIONAL,
+        DIFERENCIAL,
+        ACKERMKAN
+    };
+
+    interface IModeloInterface
+    {
+        void startModel();
+        void setSpeed(int goalSpeed);
+        int getSpeed();
+        void setDirection(int goalDir);
+        int getDirection();
+        void stopModel();
+    }
+}
